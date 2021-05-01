66c4745bed7e77c5874e131ae027448bc7b4718e
Karina Souza <souzakarina098@gmail.com>
2021-04-30 23:21:04 -0300

    add consumo de API

--- blogPessoal/src/app/cadastrar/cadastrar.component.ts
+++ blogPessoal/src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../model/User';
+import { UserLogin } from '../model/UserLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar',
@@ -6,10 +10,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastrar.component.css']
 })
 export class CadastrarComponent implements OnInit {
+  user: User= new User
+  confirmarSenha: string
+  tipoUsuario: string
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+    ) { }
+
+  ngOnInit() {
+    window.scroll(0,0)
+
+  }
+  confirmSenha(event: any){
+    this.confirmarSenha = event.target.value
+
+  }
+  tipoUser(event: any){
+    this.tipoUsuario = event.target.value
+
+  }
+  cadastrar(){
+    this.user.tipo = this.tipoUsuario
+
+    if(this.user.senha != this.confirmarSenha){
+      alert("As senhas não conferem!")
+
+    }else{
+      this.authService.cadastrar(this.user).subscribe((resp: User)=>{
+        this.user = resp
+        this.router.navigate(['/entrar'])
+        alert("Usuário cadastrado com sucesso!")
+      })
+
+    }
+
   }
 
 }
